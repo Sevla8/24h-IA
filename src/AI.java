ab6e8f98237e9d25f5389005bf3bfa3696ab9ec0
Twaninja <twan.gaudy@gmail.com>
2019-05-25 03:37:36 +0200

    voial

--- src/AI.java
+++ src/AI.java
@@ -2,52 +2,49 @@ import java.awt.Point;
 import java.util.*;
 
 public class AI {
-    private Board board;
-    private Regles regle;
+	private Board board;
+	private Regles regle;
 
+	public AI(Board initialBoard) {
+		this.board = initialBoard;
+		this.regle = new Regles(initialBoard);
+	}
 
-    public AI(Board initialBoard) {
-        this.board = initialBoard;
-        this.regle = new Regles(initialBoard);
-    }
+	// public Point play() {
 
-    // public Point play() {
-        
-    // }
+	// }
 
-    // public void interpret(Point himPlay) {
-    //     if (himPlay != null) {
-    // 		this.board.put((int)himPlay.getX(), (int)himPlay.getY(), new Cell());
+	// public void interpret(Point himPlay) {
+	// if (himPlay != null) {
+	// this.board.put((int)himPlay.getX(), (int)himPlay.getY(), new Cell());
 
-    //     }
-    //     else {
+	// }
+	// else {
 
-    //     }
-    // }
+	// }
+	// }
 
-    public boolean moiDedant(Cell c) {
-    	List<Cell> list = this.getMap().get(Character.valueOf(c.getValue()));
-    	for ( Cell cell : list ) {
-    		if (cell.getState() == Cell.State.ME_PLAYED)
-    		{
-    			return true;
-    		}
-    	}
-    	return false;
-    }
+	public boolean moiDedant(Cell c) {
+		List<Cell> list = this.getMap().get(Character.valueOf(c.getValue()));
+		for (Cell cell : list) {
+			if (cell.getState() == Cell.State.ME_PLAYED) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public boolean luiDedant(Cell c) {
-    	List<Cell> list = this.getMap().get(Character.valueOf(c.getValue()));
-    	for ( Cell cell : list ) {
-    		if (cell.getState() == Cell.State.HIM_PLAYED)
-    		{
-    			return true;
-    		}
-    	}
-    	return false;
-    }
+	public boolean luiDedant(Cell c) {
+		List<Cell> list = this.getMap().get(Character.valueOf(c.getValue()));
+		for (Cell cell : list) {
+			if (cell.getState() == Cell.State.HIM_PLAYED) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public HashMap<Character, List<Cell>> getMap() {
+	public HashMap<Character, List<Cell>> getMap() {
 		HashMap<Character, List<Cell>> hashMap = new HashMap<Character, List<Cell>>();
 		for (int i = 0; i < this.board.getCells().length; i += 1) {
 			for (int j = 0; j < this.board.getCells()[i].length; j += 1) {
@@ -56,10 +53,9 @@ public class AI {
 					List<Cell> list = new ArrayList<Cell>(hashMap.get(this.board.getCells()[i][j]));
 					list.add(this.board.getCells()[i][j]);
 					hashMap.put(this.board.getCells()[i][j].getValue(), list);
-				}
-				else {
-    				List<Cell> list = new ArrayList<Cell>();
-    				list.add(this.board.getCells()[i][j]);
+				} else {
+					List<Cell> list = new ArrayList<Cell>();
+					list.add(this.board.getCells()[i][j]);
 					hashMap.put(this.board.getCells()[i][j].getValue(), list);
 				}
 			}
@@ -86,19 +82,15 @@ public class AI {
 		return list;
 	}
 
-	/*public Cell sortList(List<Cell> list) {
-		int max = 0;
-		Cell tmp = null;
-		for (Cell cell : list) {
-			if (this.getMap().get(Character.valueOf(cell.getValue())).size() > max) {
-				tmp = cell;
-				max = this.getMap().get(Character.valueOf(cell.getValue())).size();
-			}
-		}
-		return tmp;
-	}*/
+	/*
+	 * public Cell sortList(List<Cell> list) { int max = 0; Cell tmp = null; for
+	 * (Cell cell : list) { if
+	 * (this.getMap().get(Character.valueOf(cell.getValue())).size() > max) { tmp =
+	 * cell; max = this.getMap().get(Character.valueOf(cell.getValue())).size(); } }
+	 * return tmp; }
+	 */
 
-	public Cell jouerList(List<Cell> list){
+	public Cell jouerList(List<Cell> list) {
 		int max_lui = 0;
 		int max_moi = 0;
 		Cell lui = null;
@@ -108,61 +100,45 @@ public class AI {
 		Cell tmp = null;
 
 		for (Cell cell : list) {
-			//Le plus grand personne dedant
+			// Le plus grand personne dedant
 			if (!this.moiDedant(cell) && !this.luiDedant(cell)
-				&& this.getMap().get(Character.valueOf(cell.getValue())).size() > max_moi)
-			{
+					&& this.getMap().get(Character.valueOf(cell.getValue())).size() > max_moi) {
 				moi = cell;
 				max_moi = this.getMap().get(Character.valueOf(cell.getValue())).size();
 			}
-			//Le plus grand lui seul dedant
+			// Le plus grand lui seul dedant
 			if (!this.moiDedant(cell) && this.luiDedant(cell)
-				&& this.getMap().get(Character.valueOf(cell.getValue())).size() > max_lui)
-			{
+					&& this.getMap().get(Character.valueOf(cell.getValue())).size() > max_lui) {
 				lui = cell;
 				max_lui = this.getMap().get(Character.valueOf(cell.getValue())).size();
 			}
-			//Le plus grand tout court
+			// Le plus grand tout court
 			if (this.getMap().get(Character.valueOf(cell.getValue())).size() > max_tmp) {
 				tmp = cell;
 				max_tmp = this.getMap().get(Character.valueOf(cell.getValue())).size();
 			}
 		}
 
-		if (lui == null && moi == null)
-		{
+		if (lui == null && moi == null) {
 			return tmp;
-		}
-		else
-		{
-			if (moi == null)
-			{
+		} else {
+			if (moi == null) {
 				return lui;
-			}
-			else if (lui == null)
-			{
+			} else if (lui == null) {
 				return moi;
-			}
-			else if (max_moi > max_lui)
-			{
+			} else if (max_moi > max_lui) {
 				return moi;
-			}
-			else
-			{
+			} else {
 				return lui;
 			}
 		}
 	}
 
-	public Cell jouerOuNon()
-	{
-		if (this.regle.regle6())
-		{
-			this.regle.setNbCoups(this.regle.getNbCoups()+1);
+	public Cell jouerOuNon() {
+		if (this.regle.partieContinue()) {
+			this.regle.setNbCoups(this.regle.getNbCoups() + 1);
 			return this.jouerList(this.possibylities());
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
