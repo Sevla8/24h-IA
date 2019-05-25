9d25c79ab3484530f8ca37e68d8cad867d84976a
Terbah <dorian.terb@gmail.com>
2019-05-25 03:30:29 +0200

    Commit

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,7 @@ import java.awt.Point;
 public class Application {
 
 	public static void main(String[] args) {
-		Client.initialize();
+        Client.initialize();
 		System.out.println("test");
 		boolean first = Client.instance().isFirstPlayer();
 		Board board = Client.instance().recieveMap();
@@ -15,25 +15,23 @@ public class Application {
 			Client.State state = Client.instance().getNextState();
 			if(state != null) {
 				if(state.equals(Client.State.IS_MINE_TURN)) {
-
 					//Point point = ai.play();
 					Point point = new Point(2, 4);
 					Client.instance().sendPoint(point.x, point.y);
 
 				} else if(state.equals(Client.State.IS_OPPONENT_TURN)) {
-
+                    System.out.println("Is opponent turn");
 					Point point = Client.instance().getOpponentPoint();
 					//ai.interpret(point);
 
 				} else if(state.equals(Client.State.IS_ILLEGAL_OPPONENT_SHOT)) {
-					
+                    System.out.println("Is illegal opponent shot");					
 					//ai.interpret(null);
 
 				} else if(state.equals(Client.State.IS_MINE_SHOT_ILLEGAL)) {
-					
+                    System.out.println("Is mine illegal shot");															
 					//Do nothing
 					System.err.println("Missed shot");
-
 				} else if(state.equals(Client.State.IS_END_GAME)) {
 					
 					break;
